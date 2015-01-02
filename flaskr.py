4ef59395fd3a1e707c82736fc40c4284206a7e91
xdbaqiao <yexj061761@gmail.com>
2015-01-02 22:16:33 +0000

    flaskr: add sqlite3 database initial

--- flaskr.py
+++ flaskr.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sqlite3
+from contextlib import closing
 from flask import Flask, url_for, request, session, g, redirect,\
         abort, render_template, flash
 
@@ -16,6 +17,12 @@ app.config.from_object(__name__)
 
 def databse_connect():
     return sqllit3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(databse_connect()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
     app.run()
