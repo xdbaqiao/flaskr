5ff1f2a67c4e3974fd65958b3eb9e9ad1a2801fc
xdbaqiao <yexj061761@gmail.com>
2015-01-02 22:08:11 +0000

    flaskr: running this app

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 # coding: utf-8
+
+import sqlite3
 from flask import Flask, url_for, request, session, g, redirect,\
         abort, render_template, flash
 
@@ -9,9 +11,11 @@ SECREY_KEY = '2be7cb11cb3a5aa42256822d4fbd48f2'
 USERNAME = 'admin'
 PASSWORD = 'admin123'
 
-app = Falsk(__name__)
+app = Flask(__name__)
 app.config.from_object(__name__)
 
-
+def databse_connect():
+    return sqllit3.connect(app.config['DATABASE'])
 
 if __name__ == '__main__':
+    app.run()
