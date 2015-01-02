76b18e2731be43cf7ccb6ba04ce285cf569ca5a1
xdbaqiao <yexj061761@gmail.com>
2015-01-02 23:45:31 +0000

    flaskr: debug OK

--- flaskr.py
+++ flaskr.py
@@ -8,7 +8,7 @@ from flask import Flask, url_for, request, session, g, redirect,\
 
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
-SECREY_KEY = '2be7cb11cb3a5aa42256822d4fbd48f2'
+SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'admin123'
 
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def databse_connect():
-    return sqllit3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
     with closing(databse_connect()) as db:
@@ -41,19 +41,19 @@ def show_entries():
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', method=['POST'])
+@app.route('/add', methods=['POST'])
 def add_entries():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entiries (title, text) value (?, ?)', [request.form['title'], request, form['text']])
+    g.db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
     g.db.commit()
     flash('Your new entry was successfully posted.')
     return redirect(url_for('show_entries'))
 
-@app.route('/login', method=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error=None
-    if request.method='POST':
+    if request.method=='POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
         elif request.form['password'] != app.config['PASSWORD']:
@@ -62,7 +62,7 @@ def login():
             session['logged_in'] = True
             flash('You are logged in!')
             return redirect(url_for('show_entries'))
-    return render_templates('login.html', error=error)
+    return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
